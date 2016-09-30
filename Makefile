09001e3d621f0bdb0e31d66e13e1341f0b460923
coxm <coxm@users.noreply.github.com>
2016-09-30 23:43:59 +0100

    Add tests for events (still need test runner)

--- Makefile
+++ Makefile
@@ -1,7 +1,10 @@
-ts_src_dir=src
-ts_build_dir=build/js
+lib_src_dir=src
+lib_build_dir=build/js
 
-ts_compiler_options+=--outDir $(ts_build_dir)
+test_src_dir=test
+test_build_dir=build/test
+
+ts_compiler_options+=--outDir $(lib_build_dir)
 ts_compiler_options+=--target es5
 ts_compiler_options+=--lib es7,dom
 ts_compiler_options+=--declaration
@@ -21,8 +24,14 @@ ts_compiler_options+=--newLine LF
 ts_compiler_options+=--pretty
 
 ts_compiler_bin=./node_modules/typescript/bin/tsc
-ts_sources=$(shell find $(ts_src_dir) -type f -wholename "*.ts")
-ts_outputs=$(ts_sources:$(ts_src_dir)/%.ts=$(ts_build_dir)/%.js)
+
+
+lib_sources=$(shell find $(lib_src_dir) -type f -wholename "*.ts")
+lib_outputs=$(lib_sources:$(lib_src_dir)/%.ts=$(lib_build_dir)/%.js)
+
+test_sources=$(shell find $(test_src_dir) -type f -wholename "*.ts")
+test_outputs=$(test_sources:$(test_src_dir)/%.ts=$(test_build_dir)/%.js)
+test_sources+=typings/index.d.ts
 
 
 typings_bin=node_modules/typings/dist/bin.js
@@ -33,18 +42,27 @@ $(ts_compiler_bin) $(ts_compiler_options)
 endef
 
 
-$(ts_build_dir)/%.js: $(ts_src_dir)/%.ts
+$(lib_build_dir)/%.js: $(lib_src_dir)/%.ts
+	$(tsc_compile)
+
+
+$(test_build_dir)/%.js: $(test_src_dir)/%.ts
 	$(tsc_compile)
 
 
 .PHONY: all
 all:
-	$(tsc_compile) $(ts_sources)
+	$(tsc_compile) $(lib_sources)
+
+
+.PHONY: tests
+tests:
+	$(tsc_compile) $(test_sources)
 
 
 .PHONY: clean
 clean:
-	rm -r $(ts_build_dir)
+	rm -r $(lib_build_dir)
 
 
 .PHONY: typings-install
---
+++ test/events.ts
@@ -0,0 +1,101 @@
+import {Manager, Event} from '../src/events';
+
+
+type TestEvent = Event<string, number>;
+
+
+interface Context {
+	value: number;
+}
+
+
+describe("events.Manager", (): void => {
+	const manager: Manager<string, number> = new Manager<string, number>();
+
+	describe("on method", (): void => {
+		it("sets handlers for that category", (): void => {
+			let value: number = 0;
+			manager.on('category', (ev: TestEvent): void => {
+				value = ev.data;
+			});
+			manager.fire('category', 0xf00d);
+			expect(value).toBe(0xf00d);
+		})
+		it("only sets handlers for a single category", (): void => {
+			let value: number = 0xdead
+			manager.on('this', (ev: TestEvent): void => {
+				value = ev.data;
+			});
+			manager.fire('that', 0xf00d);
+			expect(value).toBe(0xdead);
+		})
+		it("can set a context for handlers", (): void => {
+			const context: Context = { value: 0xdead };
+			function handler(this: Context, ev: TestEvent): void {
+				this.value = ev.data;
+			}
+			manager.on('category', handler, context);
+			manager.fire('category', 0xf00d);
+			expect(context.value).toBe(0xf00d);
+		})
+		it("can limit the number of times a handler is called", (): void => {
+			const limit: number = 3;
+			let value: number = 0;
+			manager.on('category', (ev: TestEvent): void => {
+				value = ev.data;
+			}, null, limit);
+
+			for (let i: number = 0; i < limit; ++i) {
+				manager.fire('category', i);
+				expect(value).toBe(i);
+			}
+			manager.fire('category', limit);
+			expect(value).toBe(limit - 1);
+		});
+	})
+
+	describe("once method", (): void => {
+		it("sets a one-time event handler", (): void => {
+			let value: number = 0xdead;
+			manager.once('category', (ev: TestEvent): void => {
+				value = ev.data;
+			});
+			manager.fire('category', 1);
+			expect(value).toBe(1);
+			manager.fire('category', 2);
+			expect(value).toBe(1);
+		})
+	})
+
+	describe("off method", (): void => {
+		it("cancels existing handlers", (): void => {
+			let callCount: number = 0;
+			function handler(ev: TestEvent): void {
+				++callCount;
+			}
+			manager.on('category', handler);
+			manager.off('category', handler);
+			manager.fire('category', 1);
+			expect(callCount).toBe(0);
+		})
+		it("does nothing for nonexistent handlers", (): void => {
+			expect((): void => {
+				manager.off('nope', (): void => {});
+			}).not.toThrow();
+		})
+		it("doesn't cancel handlers with different contexts", (): void => {
+			function handler(this: Context, ev: TestEvent): void {
+				this.value = ev.data;
+			}
+			const category: string = 'category';
+			const context1: Context = { value: 1 };
+			const context2: Context = { value: 2 };
+			manager.on(category, handler, context1);
+			manager.on(category, handler, context2);
+			manager.off(category, handler, context2);
+			manager.fire(category, 3);
+			expect(context1.value).toBe(3);
+			expect(context2.value).toBe(2);
+		})
+	})
+})
