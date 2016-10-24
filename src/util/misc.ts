ea15ecae4cac3642cd0d399d02cc34fdaaeba467
coxm <coxm@users.noreply.github.com>
2016-10-24 20:52:19 +0100

    Add isReal, isInt, toInt, randIntInRange

--- src/util/misc.ts
+++ src/util/misc.ts
@@ -9,8 +9,28 @@ export function identity<T>(t: T): T {
 }
 
 
+export function isReal(x: number): boolean {
+	return x === +x;
+}
+
+
+export function isInt(x: number): boolean {
+	return x === (x|0);
+}
+
+
+export function toInt(x: number): number {
+	return x|0;
+}
+
+
 export function randInRange(min: number, max: number): number {
 	return Math.random() * (max - min) - min;
+}
+
+
+export function randIntInRange(min: number, max: number): number {
+	return toInt(randInRange(min, max));
 }
 
 
