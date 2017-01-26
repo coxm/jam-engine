df952e75da11826f3beb586de8c54d37b2c6a0bb
coxm <coxm@users.noreply.github.com>
2017-01-26 12:39:24 +0000

    Expand range utils

--- src/util/range.ts
+++ src/util/range.ts
@@ -1,31 +1,60 @@
-import {numberOr} from 'jam/util/misc';
-
-
 export interface LengthRange {
-	readonly gte: number;
+	readonly beg: number;
 	readonly len: number;
+	readonly by?: number;
 }
 
 
 export interface IntervalRange {
-	readonly gte: number;
-	readonly lt: number;
+	readonly beg: number;
+	readonly pre: number;
+	readonly by?: number;
 }
 
 
 export type Range = LengthRange | IntervalRange;
 
 
+/**
+ * Create a range iterator.
+ *
+ * @param range.gte the (inclusive) lower bound.
+ * @param range.len optionally specify a maximum length.
+ * @param
+ * @param range.by the step (can be negative); defaults to 1.
+ */
 export function* range(range: Range): IterableIterator<number> {
-	for (
-		let i: number = range.gte,
-			end: number = numberOr(
-				(<IntervalRange> range).lt,
-				(<LengthRange> range).len + i
-			);
-		i < end;
-		++i
-	) {
+	const step: number = range.by || 1;
+	let cont: (i: number) => boolean;
+	if ((<IntervalRange> range).pre) {
+		const pre: number = (<IntervalRange> range).pre;
+		cont = (step < 0
+			?	((i: number): boolean => i > pre)
+			:	((i: number): boolean => i < pre)
+		);
+	}
+	else {
+		let count: number = (<LengthRange> range).len;
+		cont = i => --count >= 0;
+	}
+
+	for (let i: number = range.beg; cont(i); i += step) {
 		yield i;
+	}
+}
+
+
+export function* combine(ranges: Iterable<number|Range>)
+	: IterableIterator<number>
+{
+	for (let item of ranges) {
+		if (item === +item) {
+			yield item;
+		}
+		else {
+			for (let i of range(<Range> item)) {
+				yield i;
+			}
+		}
 	}
 }
--- test/util/range.ts
+++ test/util/range.ts
@@ -1,23 +1,70 @@
-import {range, Range} from 'jam/util/range';
+import {Range, range, combine} from 'jam/util/range';
 
 
-describe("rangeIndices", (): void => {
+describe("range", (): void => {
 	function test(msg: string, input: Range, output: number[]): void {
 		it(msg, (): void => {
 			const iter = range(input);
-			expect([...iter]).toEqual(output);
+			const array = [...iter];
+			expect(array).toEqual(output);
 		});
 	}
 
 	test(
 		"works for length ranges",
-		{gte: -4, len: 6},
+		{beg: -4, len: 6},
 		[-4, -3, -2, -1, 0, 1]
 	);
 
 	test(
 		"works for interval ranges",
-		{gte: -4, lt: 2},
+		{beg: -4, pre: 2},
 		[-4, -3, -2, -1, 0, 1]
 	);
+
+	test(
+		"can step by arbitrary positive amounts (length)",
+		{beg: -4, len: 4, by: 1.5},
+		[-4, -2.5, -1, 0.5]
+	);
+
+	test(
+		"can step by arbitrary negative amounts (length)",
+		{beg: 2, pre: -4, by: -1.5},
+		[2, 0.5, -1, -2.5]
+	);
+
+	test(
+		"can step by arbitrary positive amounts (length)",
+		{beg: -4, len: 4, by: 1.5},
+		[-4, -2.5, -1, 0.5]
+	);
+
+	test(
+		"can step by arbitrary negative amounts (length)",
+		{beg: 2, len: 4, by: -1.5},
+		[2, 0.5, -1, -2.5]
+	);
+});
+
+
+describe("combine", (): void => {
+	it("combines numbers", (): void => {
+		expect([...combine([1, 2, 3])]).toEqual([1, 2, 3]);
+	});
+	it("combines ranges", (): void => {
+		expect([...combine([
+			{beg: 1, len: 3},
+			{beg: 5, pre: 8}
+		])])
+		.toEqual([1, 2, 3, 5, 6, 7]);
+	});
+	it("combines a mixture of numbers and ranges", (): void => {
+		expect([...combine([
+			{beg: 1, len: 3},
+			4,
+			{beg: 5, pre: 8}
+		])])
+		.toEqual([1, 2, 3, 4, 5, 6, 7]);
+	});
 });
