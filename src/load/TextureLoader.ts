159c9ce779dd3212740f2855043e2086d2c0a889
coxm <coxm@users.noreply.github.com>
2017-02-25 11:50:46 +0000

    Make TextureLoader more resilient
    Prevent crashes when requesting a texture while
    PIXI.loader is loading other resources.

--- src/load/TextureLoader.ts
+++ src/load/TextureLoader.ts
@@ -1,6 +1,65 @@
 import {FileLoader} from './FileLoader';
 
 
+/**
+ * Load an array of textures by absolute path.
+ *
+ * Uses {@link PIXI.loader} to load textures, but waits until the current load
+ * batch (if any) has finished, to avoid conflicts (`ResourceLoader` throws an
+ * error otherwise).
+ *
+ * @param paths the array of texture paths.
+ * @returns a promise which resolves with the loaded textures.
+ */
+function loadTextures(paths: string[]): Promise<PIXI.Texture[]> {
+	return new Promise((resolve, reject): void => {
+		if (!PIXI.loader.loading) {
+			return fetchTextures(paths, resolve, reject);
+		}
+
+		(<any> PIXI.loader).onComplete.add(
+			fetchTextures.bind(null, paths, resolve, reject)
+		);
+	});
+}
+
+
+/**
+ * Fetch an array of textures using {@link PIXI.loader}.
+ *
+ * @param paths the texture paths.
+ * @param success the success callback.
+ * @param failure the failure callback.
+ */
+function fetchTextures(
+	paths: string[],
+	success: (value: PIXI.Texture[]) => void,
+	failure: (value: any) => void
+)
+	: void
+{
+	let count: number = 0;
+	for (let path of paths) {
+		if (!PIXI.utils.TextureCache[path]) {
+			PIXI.loader.add(path);
+			++count;
+		}
+	}
+
+	function done(): void {
+		success(paths.map(path => PIXI.utils.TextureCache[path]));
+	}
+
+	if (count === 0) {
+		return done();
+	}
+
+	// onError missing from definitions.
+	(<any> PIXI.loader).onError.add(failure);
+	PIXI.loader.load(done);
+}
+
+
 export class TextureLoader extends FileLoader {
 	cached(relpath: string): PIXI.Texture {
 		return PIXI.utils.TextureCache[this.abspath(relpath)];
@@ -8,25 +67,10 @@ export class TextureLoader extends FileLoader {
 
 	texture(relpath: string): Promise<PIXI.Texture> {
 		const abspath: string = this.abspath(relpath);
-		return new Promise((resolve, reject): void => {
-			function done(): void {
-				resolve(PIXI.utils.TextureCache[abspath]);
-			}
-
-			if (PIXI.utils.TextureCache[abspath]) {
-				done();
-			}
-			else {
-				PIXI.loader.add(abspath).load(done);
-			}
-		});
+		return loadTextures([abspath]).then(textures => textures[0]);
 	}
 
-	textures(...relpaths: string[]): Promise<void> {
-		return new Promise((resolve, reject): void => {
-			PIXI.loader
-			.add(relpaths.map(this.abspath.bind(this)))
-			.load(resolve);
-		});
+	textures(...relpaths: string[]): Promise<PIXI.Texture[]> {
+		return loadTextures(relpaths.map(rel => this.abspath(rel)));
 	}
 }
