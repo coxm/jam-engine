fba50e9673549b74b58b67faae5fd1235370b7f9
coxm <coxm@users.noreply.github.com>
2017-03-03 13:20:02 +0000

    Add MultiRender actor component

---
+++ src/actors/cmp/MultiRender.ts
@@ -0,0 +1,24 @@
+import {Actor, Component} from '../Actor';
+
+
+export class MultiRender implements Component {
+	key: string;
+
+	readonly renderable: PIXI.Container;
+
+	constructor(
+		children: PIXI.DisplayObject[],
+		public readonly actorID: symbol
+	) {
+		this.renderable = new PIXI.Container();
+		for (let i = 0, len = children.length; i < len; ++i) {
+			this.renderable.addChild(children[i]);
+		}
+	}
+
+	onAdd(actor: Actor): void {
+	}
+
+	onRemove(actor: Actor): void {
+	}
+}
