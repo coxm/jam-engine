52ff7cb981996f1bfef3a0b142662e06149b919d
coxm <coxm@users.noreply.github.com>
2017-08-06 13:09:05 +0100

    Fix texture loading

--- src/load/textures.ts
+++ src/load/textures.ts
@@ -1,54 +1,33 @@
+import {Pool} from 'jam/util/Pool';
+
+
+const pool = new Pool<PIXI.loaders.Loader>({
+	create: () => new PIXI.loaders.Loader(),
+	reset(loader: PIXI.loaders.Loader) {
+		(loader as any).removeAllListeners();
+		return loader.reset();
+	},
+	maxIdle: 3,
+});
+
+
 /**
  * Load an array of textures by absolute path.
  *
- * Uses {@link PIXI.loader} to load textures, but waits until the current load
- * batch (if any) has finished, to avoid conflicts (`ResourceLoader` throws an
- * error otherwise).
+ * Uses {@link PIXI.loaders.Loader} instances to load textures.
  *
  * @param paths the array of texture paths.
  * @returns a promise which resolves with the loaded textures.
  */
-export function loadTextures(paths: string[]): Promise<PIXI.Texture[]> {
-	return new Promise((resolve, reject): void => {
-		if (!PIXI.loader.loading) {
-			return fetchTextures(paths, resolve, reject);
-		}
-
-		(<any> PIXI.loader).onComplete.add(
-			fetchTextures.bind(null, paths, resolve, reject)
-		);
-	});
-}
-
-
-/**
- * Fetch an array of textures using {@link PIXI.loader}.
- *
- * @param paths the texture paths.
- * @param success the success callback.
- * @param failure the failure callback.
- */
-export function fetchTextures(
-	paths: string[],
-	success: (value: PIXI.Texture[]) => void,
-	failure: (value: any) => void
-)
-	: void
+export const loadTextures = (paths: string[], baseUrl: string = '')
+	: Promise<PIXI.Texture[]> =>
 {
-	const loader = new PIXI.loaders.Loader();
-
-	for (const url of paths) {
-		if (!PIXI.utils.TextureCache[url]) {
-			loader.add(url);
-		}
-	}
-
-	(<PIXI.loaders.Loader>
-		loader
-		.once('error', failure)
-		.once('complete', () => success(
-			paths.map(p => PIXI.utils.TextureCache[p])
-		))
-	)
-	.load();
-}
+	const loader = pool.get();
+	loader.baseUrl = baseUrl;
+	loader.add(paths);
+	return new Promise((resolve, reject): void => {
+		(loader.on('error', reject) as PIXI.loaders.Loader).load((): void => {
+			resolve(paths.map(p => PIXI.utils.TextureCache[p]));
+		});
+	});
+};
